6fd6661352ae357e8aa8cd63fa2bf8c488d6c707
Yusuke Murakami <tropical.inu@gmail.com>
2018-10-01 18:26:13 +0900

    front_design_test_main

---
+++ app/assets/javascripts/mypage_sidebar.js
@@ -0,0 +1,19 @@
+$(function() {
+  $(".mypage-nav-list-item").on("mouseover", function() {
+    $(this).css('background', '#EEEEEE');
+    var faAngleRight = $(this).find(".fa-angle-right");
+    $(faAngleRight).css('color', 'black');
+    $(faAngleRight).animate({
+    'right': '12px',
+  }, 0, 'easing');
+  });
+
+ $(".mypage-nav-list-item").on("mouseout", function(){
+    $(this).css('background', 'white');
+    var faAngleRight = $(this).find(".fa-angle-right");
+    $(faAngleRight).css('color', '#ccc');
+    $(faAngleRight).animate({
+    'right': '16px',
+  }, 0, 'easing');
+  });
+});
