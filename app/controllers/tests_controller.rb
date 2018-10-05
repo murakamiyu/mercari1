bd4b423f406d6868cad88b32963b8c193e154f50
Yusuke Murakami <tropical.inu@gmail.com>
2018-10-05 12:21:14 +0900

    add_new_account_design

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,43 @@
 class TestsController < ApplicationController
+  def index
+  end
+
+  def item_detail
+  end
+
+  def order_status
+  end
+
+  def purchase_pre_confirmation
+    render :layout => false
+  end
+
+  def purchase_confirmation
+    render :layout => false
+  end
+
+  def account
+    render
+  end
+
+  def sms_confirmation
+    render :layout => nil
+  end
+
+  def adress_new
+    render :layout => nil
+  end
+
+  def credit_new
+    render :layout => nil
+  end
+
+  def account_completion
+    render :layout => nil
+  end
+
+  def putting_item
+    render :layout => nil
+  end
+
 end
