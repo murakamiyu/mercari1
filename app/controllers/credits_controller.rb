3836901f9e23fc18849baef1bbf51fbb6dc988df
User <user@yuzanoMacBook-ea.local>
2018-10-11 21:14:08 +0900

    edit_credits_controller

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,4 +1,5 @@
 class CreditsController < ApplicationController
+  before_action :set_credit, only: [:destroy]
 
   def index
     @credit = Credit.find_by(user_id: current_user.id)
@@ -9,18 +10,24 @@ class CreditsController < ApplicationController
   end
 
   def create
-    Credit.create(num: credit_params[:num], expire_month: credit_params[:expire_month], expire_year: credit_params[:expire_year], security_code: credit_params[:security_code], user_id: current_user.id)
+    # Credit.create(num: credit_params[:num], expire_month: credit_params[:expire_month], expire_year: credit_params[:expire_year], security_code: credit_params[:security_code], user_id: current_user.id)
+    Credit.create(credit_params)
     redirect_to mains_top_path
   end
 
   def destroy
-    credit = Credit.find(params[:id])
-    credit.destroy
+    # credit = Credit.find(params[:id])
+    @credit.destroy
     redirect_to mains_top_path
   end
 
   private
+
   def credit_params
-    params.require(:credit).permit(:num, :expire_month, :expire_year, :security_code)
+    params.require(:credit).permit(:num, :expire_month, :expire_year, :security_code).merge(user_id: current_user.id)
+  end
+
+  def set_credit
+    @credit = Credit.find(params[:id])
   end
 end
