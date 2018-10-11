33d76a7052c3d1370429ebfb038c35e9debca8d3
森田賢 <moritaken@moritakennoMacBook-puro.local>
2018-10-11 20:08:34 +0900

    住所の追加

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,36 @@
+class AddressesController < ApplicationController
+  before_action :set_address, only: [:edit, :update]
+
+  def new
+    @user = current_user
+    @address = Address.where(user_id: @user.id).first_or_initialize
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to mains_top_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @address.update(address_params)
+      redirect_to mains_top_path
+    else
+      render :new
+    end
+  end
+
+
+private
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:area, :city, :postal_code, :address1, :address2).merge(user_id: current_user.id)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009071100) do
+ActiveRecord::Schema.define(version: 20181011085110) do
+
+  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "area"
+    t.string   "postal_code"
+    t.string   "city"
+    t.string   "address1"
+    t.string   "address2"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "name",       limit: 65535, null: false
